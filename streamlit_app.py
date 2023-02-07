c699cbdc6db4ebc4b6b1fea3c49e9b88fbfac850
kairolrol <118028217+kairolrol@users.noreply.github.com>
2023-02-07 13:36:59 -0800

    stopping before snowflake portion to troubleshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # calls on the new variable
 streamlit.dataframe(fruityvice_normalized)
 
+# don't run anyhting past here while we troubleshoot
+streamlit.stop()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
